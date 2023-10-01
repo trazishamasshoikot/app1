a91adc131be65c1d7906f4799ec5d6b96c88f4cd
trazishamasshoikot <sabbirahame012@gmail.com>
2023-10-01 23:20:41 +0600

    commit one

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,138 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(ImageFeedApp());
+void main() {
+  runApp(MyApp());
+}
 
-class ImageFeedApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Image Feed'),
-        ),
-        body: ImageFeed(),
+      home: OrientationBuilder(
+        builder: (context, orientation) {
+          return orientation == Orientation.landscape
+              ? LandscapeProfileScreen()
+              : PortraitProfileScreen();
+        },
       ),
     );
   }
 }
 
-class ImageFeed extends StatelessWidget {
-
+class PortraitProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
       ),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Card(
-          child: Image.network("https://via.placeholder.com/150"),
-        );
-      },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 80.0,
+              backgroundImage: NetworkImage('https://shorturl.at/oqMY7'),
+            ),
+            SizedBox(height: 16.0),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.black,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'John Doe\n',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: 'Designer',
+                    style: TextStyle(fontSize: 18.0, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Some details about John Doe',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 16.0),
+            Expanded(
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: List.generate(
+                    6,
+                        (index) => Image.network(
+                      'https://shorturl.at/oqMY7',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LandscapeProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80.0,
+                backgroundImage: NetworkImage('https://shorturl.at/oqMY7'),
+              ),
+              SizedBox(height: 16.0),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'John Doe\n',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: 'Designer',
+                      style: TextStyle(fontSize: 18.0, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Some details about John Doe',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 16.0),
+              GridView.count(
+                crossAxisCount: 3,
+                children: List.generate(
+                  6,
+                      (index) => Image.network(
+                    'https://shorturl.at/oqMY7', // Replace with the actual image URL
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
