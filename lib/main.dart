42db5b1aecef6b5b3d6ddb8cb7d2e556095b0eaa
trazishamasshoikot <sabbirahame012@gmail.com>
2023-10-13 16:08:04 +0600

    commit one

--- lib/main.dart
+++ lib/main.dart
@@ -1,157 +1,148 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class Item {
-  String name;
-  String color;
-  String size;
-  double price;
-  int quantity;
-
-  Item({
-    required this.name,
-    required this.color,
-    required this.size,
-    required this.price,
-    this.quantity = 0,
-  });
-}
-
-class CartItem {
-  Item item;
-  int quantity;
-
-  CartItem(this.item, this.quantity);
-}
-
+void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.blue),
+      color: Colors.blue,
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home:HomeActivity(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Item> items = [
-    Item(name: 'Shirt', color: 'Red', size: 'M', price: 20.0),
-    Item(name: 'Pant', color: 'Blue', size: 'L', price: 25.0),
-    Item(name: 'Lungi', color: 'Green', size: 'S', price: 15.0),
-  ];
-
-  List<CartItem> cart = [];
-
-  Map<Item, int> productCount = {};
-
-  void addToCart(Item item) {
-    setState(() {
-      int index = cart.indexWhere((cartItem) => cartItem.item == item);
-      if (index != -1) {
-        cart[index].quantity++;
-      } else {
-        cart.add(CartItem(item, 1));
-      }
-      updateProductCount();
-    });
-  }
-
-  void removeFromCart(Item item) {
-    setState(() {
-      int index = cart.indexWhere((cartItem) => cartItem.item == item);
-      if (index != -1) {
-        if (cart[index].quantity > 1) {
-          cart[index].quantity--;
-        } else {
-          cart.removeAt(index);
-        }
-      }
-      updateProductCount();
-    });
-  }
-
-  void updateProductCount() {
-    productCount.clear();
-    for (CartItem cartItem in cart) {
-      productCount[cartItem.item] = cartItem.quantity;
-    }
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+  MySnackBar(message,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Bag'),
+        title: Text("Inventory App"),
+        titleSpacing: 0,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 0,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(onPressed: () {
+            MySnackBar("I am Comment", context);
+          }, icon: Icon(Icons.comment)),
+          IconButton(onPressed: () {
+            MySnackBar("I am Search", context);
+          }, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {
+            MySnackBar("I am Settings", context);
+          }, icon: Icon(Icons.settings)),
+          IconButton(onPressed: () {
+            MySnackBar("I am Email", context);
+          }, icon: Icon(Icons.email))
+        ],
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          Item item = items[index];
-          int quantityInCart = productCount[item] ?? 0;
-          return ListTile(
-            leading: Image.asset('assets/${item.name.toLowerCase()}.jpg'),
-            title: Text(item.name),
-            subtitle: Text('Color: ${item.color}, Size: ${item.size}, Price: \$${item.price.toStringAsFixed(2)}'),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.remove),
-                  onPressed: () => removeFromCart(item),
-                ),
-                Text(quantityInCart.toString()),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () => addToCart(item),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.deepPurple,
         onPressed: () {
-          final snackBar = SnackBar(
-            content: Text('Congratulations! Your total amount is \$${calculateTotalPrice().toStringAsFixed(2)}'),
-          );
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          MySnackBar("I am action Buttom", context);
         },
-        label: Text('CHECK OUT'),
-        icon: Icon(Icons.shopping_cart),
       ),
-      // Show the total cost here
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "profil"),
+        ],
+        onTap: (int index) {
+          if (index == 0) {
+            MySnackBar("I am home bottom manu", context);
+          }
+          if (index == 1) {
+            MySnackBar("I am home bottom manu", context);
+          }
+          if (index == 2) {
+            MySnackBar("I am home bottom manu", context);
+          }
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
           children: [
-            Text('Total: \$${calculateTotalPrice().toStringAsFixed(2)}', style: TextStyle(fontSize: 20)),
+            DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.black),
+                  accountName: Text("Sabbir Ahamed Shoikot"),
+                  accountEmail: Text("Info@sabbirahame012.com"),
+                  currentAccountPicture: Image.network("https://scontent.fdac27-1.fna.fbcdn.net/v/t39.30808-6/380237102_1988774341493941_6169131301116223720_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFKCi7VFDuJlnRCnOwzrjGS_DjF6OqwQvf8OMXo6rBC99YMKCeymYmZwnZrwJ8TV1aGFK0FZqmli0jiWDgUCM6V&_nc_ohc=JWzC7sGV3bIAX_f43Oq&_nc_ht=scontent.fdac27-1.fna&oh=00_AfAjdNJlzW9GGb5yUST0OLkSMK2Jc3p_qP9tiYJFk3hOYw&oe=652DF012"),
+                )
+            ),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {
+                  MySnackBar("I am Home", context);
+                }),
+            ListTile(
+                leading: Icon(Icons.contact_mail),
+                title: Text("Email"),
+                onTap: () {
+                  MySnackBar("I am Email", context);
+                }),
+            ListTile(
+                leading: Icon(Icons.phone),
+                title: Text("Phone"),
+                onTap: () {
+                  MySnackBar("I am Phone", context);
+                })
+          ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.black),
+                  accountName: Text("Sabbir Ahamed Shoikot"),
+                  accountEmail: Text("Info@sabbirahame012.com"),
+                  currentAccountPicture: Image.network("https://scontent.fdac27-1.fna.fbcdn.net/v/t39.30808-6/380237102_1988774341493941_6169131301116223720_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFKCi7VFDuJlnRCnOwzrjGS_DjF6OqwQvf8OMXo6rBC99YMKCeymYmZwnZrwJ8TV1aGFK0FZqmli0jiWDgUCM6V&_nc_ohc=JWzC7sGV3bIAX_f43Oq&_nc_ht=scontent.fdac27-1.fna&oh=00_AfAjdNJlzW9GGb5yUST0OLkSMK2Jc3p_qP9tiYJFk3hOYw&oe=652DF012"),
+                )
+            ),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {
+                  MySnackBar("I am Home", context);
+                }),
+            ListTile(
+                leading: Icon(Icons.contact_mail),
+                title: Text("Email"),
+                onTap: () {
+                  MySnackBar("I am Email", context);
+                }),
+            ListTile(
+                leading: Icon(Icons.phone),
+                title: Text("Phone"),
+                onTap: () {
+                  MySnackBar("I am Phone", context);
+                })
           ],
         ),
       ),
     );
   }
 
-  double calculateTotalPrice() {
-    double total = 0;
-    for (CartItem cartItem in cart) {
-      total += cartItem.item.price * cartItem.quantity;
-    }
-    return total;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    updateProductCount();
-  }
 }
