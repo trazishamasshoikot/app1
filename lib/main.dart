fadadc645f9d623b8c65bbb7e3232b862e594621
trazishamasshoikot <sabbirahame012@gmail.com>
2024-11-16 00:08:39 +0600

    commit one

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,89 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      darkTheme: ThemeData(primarySwatch: Colors.blue),
-      color: Colors.blue,
-      debugShowCheckedModeBanner: false,
-      home:HomeActivity(),
+      home: MyProfileScreen(),
     );
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class MyProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('My Profile'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.call),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProfileItem(
+              icon: Icons.icecream,
+              text: 'Ice cream is very delicious right?',
+            ),
+            SizedBox(height: 20),
+            ProfileItem(
+              icon: Icons.code,
+              text: 'Programming is not boring if you love it',
+            ),
+            SizedBox(height: 20),
+            ProfileItem(
+              icon: Icons.egg,
+              text: 'If you submit code directly copy from chatgpt then mark will 0',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
+class ProfileItem extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  ProfileItem({required this.icon, required this.text});
 
   @override
   Widget build(BuildContext context) {
-
-    return DefaultTabController(
-        length: 8,
-        child: Scaffold(
-         appBar: AppBar(
-          title: Text("dresli"),
-          bottom: TabBar(
-           isScrollable: true,
-          tabs: [
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",),
-            Tab(icon: Icon (Icons.home),text: "home",)
-            ],
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.purple.shade100,
+          radius: 60,
+          child: Icon(icon, size: 60, color: Colors.deepPurple),
+        ),
+        SizedBox(height: 20),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
-          body: TabBarView(
-            children: [
-
-            ],
-          ),
-      )
+      ],
     );
   }
 }
